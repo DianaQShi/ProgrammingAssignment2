b7c6f84a0289d86550845b03282200e8383fad12
dianashi <DianaQShi@gmail.com>
2020-07-23 16:17:06 -0500

    Comments were added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I completed the following two functions that cache the inverse of
+## a matrix. The first function "makeCacheMatrix" caches the inverse 
+## of a matrix. The second function "cacheSolve" computes the inverse
+## of the matrix returned by the first function.
+## The codes were adapted from the example "Caching the Mean of a
+## Vector" provided with the assignment. Thank goodness for the example!
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function that (hopefully) creates a "matrix"
+## object that can cache its inverse:
+##   set - set the matrix
+##   get - get the matrix
+##   setinmatrix - set the inverse of the matrix
+##   getinmatrix - get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,7 +28,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that computes the inverse of the matrix
+## returned by the makeCacheMatrix function. The if statement checks
+## to see if the inverse matrix has already been calculated. If so,
+## the matrix will be obtained from the cache instead of further
+## computation.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -32,3 +46,5 @@ cacheSolve <- function(x, ...) {
     x$setinmatrix(m)
     m
 }
+
+## PS: Thank you for reviewing my work!
