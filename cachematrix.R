b28f16b792240b13fa815c67c1f88a04c3e7afc1
dianashi <DianaQShi@gmail.com>
2020-07-23 15:41:16 -0500

    Finished editing function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinmatrix <- function(inmatrix) m <<- inmatrix
+  getinmatrix <- function() m
+  list(set = set, get = get,
+       setinmatrix = setinmatrix,
+       getinmatrix = getinmatrix)
 }
 
 
